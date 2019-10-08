eeeba7c418005eac82079964caa11b61f42e0f45
Vitalii Hopaniuk <Vitalii.Hopaniuk@3shape.com>
2019-10-08 17:28:35 +0300

    Add test example

---
+++ src/client/src/components/Layout/EmptyLayout/__tests__/EmptyLayout.test.tsx
@@ -0,0 +1,34 @@
+/**
+ * Absolute imports
+ */
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+
+/**
+ *
+ */
+import EmptyLayout from '../EmptyLayout';
+
+const children = <span>Test</span>;
+const renderComponent = (props = {}) =>
+  render(<EmptyLayout {...props}>{children}</EmptyLayout>);
+
+describe('<A />', () => {
+  afterEach(cleanup);
+
+  it('should match snapshot', () => {
+    const { container } = renderComponent();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render a <main> tag', () => {
+    const { container } = renderComponent();
+    expect(container.querySelector('main')).not.toBeNull();
+  });
+
+  it('should have render children', () => {
+    const { container } = renderComponent();
+    // @ts-ignore
+    expect(container.querySelector('main').children).toHaveLength(1);
+  });
+});
